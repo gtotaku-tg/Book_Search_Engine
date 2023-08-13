e53e08c5f0ffd2c3c0374fd45156e0b96bd81bf2
gtotaku-tg <124806960+gtotaku-tg@users.noreply.github.com>
2023-08-13 01:11:41 -0400

    app.js file up to date

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,34 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const auth = setContext((_, {headers}) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+}
+);
+
+const client = new ApolloClient({
+  link: auth.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -14,8 +39,11 @@ function App() {
           <Route exact path='/saved' component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
+        
       </>
     </Router>
+    </ApolloProvider>
+    
   );
 }
 
