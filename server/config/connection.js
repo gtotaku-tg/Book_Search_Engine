efb37671c730d500674d9899e42c464de5a902c6
gtotaku-tg <124806960+gtotaku-tg@users.noreply.github.com>
2023-08-13 23:25:49 -0400

    connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,6 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-
-// module.exports = mongoose.connection;
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+module.exports = mongoose.connection;
 
 
 // const mongoose = require("mongoose");
@@ -15,13 +13,13 @@
 // );
 // module.exports = mongoose.connection;
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
